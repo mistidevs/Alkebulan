845db323648ed0735ba712768ca12bec4d60a9db
mistidevs <mistidevs@gmail.com>
2024-04-17 17:14:46 +0300

    code (index.py) : Root path

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -16,7 +16,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/', methods=['GET'], strict_slashes=False)
 def api():
     """
     Status of API
