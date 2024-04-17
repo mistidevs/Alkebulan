181da533f3d31a92864ec54404945f931aacce96
mistidevs <mistidevs@gmail.com>
2024-04-17 15:05:04 +0300

    code (farmers_consumers_products.sql) : Adding a use database statement

--- farmers_consumers_products.sql
+++ farmers_consumers_products.sql
@@ -19,6 +19,7 @@
 -- Table structure for table `admins`
 --
 
+USE alkebulan_db;
 DROP TABLE IF EXISTS `admins`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
